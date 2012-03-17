ec8409d2602b221c12df0bac5fec86df8fd13a38
kiminari homma <u533u778@gmail.com>
2012-03-17 09:59:35 -0700

    added mongodb, jquery and rspec environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youme::Application.config.secret_token = '5d20c5abf75a881d95dda3f22f1b1f05452bc379086350d1e990fb4af6faf33c13a3ca3e3c60a003d00137f600a896acafd4fe064bf60028a44e120f65438797'
---
+++ lib/mongo_mapper/mongrations.rb
@@ -0,0 +1,8 @@
+#module MongoMapper
+#  class Base
+#    def self.connection
+#      MongoMapper.connection
+#    end
+#  end
+#end
+
